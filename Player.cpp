03cf3b24c773cad681816e5ac459415ab2282ca8
Star <306590769@qq.com>
2022-01-22 18:40:57 +0800

    完善了基本的移动及其判定

--- Player.cpp
+++ Player.cpp
@@ -20,186 +20,93 @@ int Player::Updata()
 	if (flag_window > 0) { flag_window++; }
 	if (flag_window >= 150) { flag_window = 0; }
 	//更新玩家操作
-	//红色玩家
-	if (flag == Red_Player) 
+	// //预选位置向上移动
+	if (input->GetKeyState(SDL_SCANCODE_W) == Key_Down || 
+		input->GetKeyState(SDL_SCANCODE_UP) == Key_Down)
 	{
-		//预选位置向上移动
-		if (input->GetKeyState(SDL_SCANCODE_W) == Key_Down) 
-		{
-			yuxuan_point.y--;
-		}
-		//预选位置向下移动
-		else if(input->GetKeyState(SDL_SCANCODE_S) == Key_Down)
-		{
-			yuxuan_point.y++;
-		}
-		//预选位置向左移动
-		else if (input->GetKeyState(SDL_SCANCODE_A) == Key_Down)
-		{
-			
-			yuxuan_point.x--;
-		}
-		//预选位置向右移动
-		else if (input->GetKeyState(SDL_SCANCODE_D) == Key_Down)
-		{
-			yuxuan_point.x++;
-		}
-		//选择
-		//选择结束时记得更新地图
-		else if (input->GetKeyState(SDL_SCANCODE_G) == Key_Down) 
-		{
-			//判断是否是玩家的回合
-			if (map->Gethuihe() % 2 == Red_Player) 
-			{
-				//地图点位未被占用
-				if (map->GetMapState(&yuxuan_point).zhan_Point == NONE)
-				{
-					//基地还没确定，优先确定基地
-					if (!jidi_point.x) {
-						map->Update(&yuxuan_point,Red_Player);
-						jidi_point.x = yuxuan_point.x * 100 - 75;
-						jidi_point.y = yuxuan_point.y * 100 - 75;
-						before_point = yuxuan_point;
-						Point* point = new Point(jidi_point);
-						point->x += 25;
-						point->y += 25;
-						move_point.emplace_back(point);//将点位加入
-					}
-					else {
-						switch (MoveIsRight()) {
-						case 1:
-							map->Update(&yuxuan_point, Blue_Player);
-							before_point = yuxuan_point;
-							move_point.emplace_back(ToMapPoint(&yuxuan_point));//将点位加入
-							break;
-						case TOO_LONG:
-							//提示玩家距离过长
-							if (flag_window == 0) {
-								flag_window++;
-								main_window->Player_Window(L"距离过长",
-									{ Getyuxuan_point().x,Getyuxuan_point().y + 20,100,30 });
-							}
-							break;
-						case THOUGH:
-							//提示玩家禁止穿线
-							if (flag_window == 0) {
-								flag_window++;
-								main_window->Player_Window(L"禁止穿线",
-									{ Getyuxuan_point().x,Getyuxuan_point().y + 20,100,30 });
-							}
-							break;
-						}
-					}
-				}
-				else
-				{
-					//点位被占用
-					if (flag_window == 0) {
-						flag_window++;
-						main_window->Player_Window(L"点位被使用",
-							{ Getyuxuan_point().x,Getyuxuan_point().y + 20,100,30 });
-					}
-				}
-			}
-			else {
-				if (flag_window == 0) {
-					flag_window++;
-					//轮到敌人
-					main_window->Player_Window(L"轮到蓝方",
-						{ Getyuxuan_point().x,Getyuxuan_point().y + 20,100,30 });
-				}
-			}
-		}
-			
+		yuxuan_point.y--;
 	}
-	//蓝色玩家
-	else 
+	//预选位置向下移动
+	else if (input->GetKeyState(SDL_SCANCODE_S) == Key_Down || 
+		input->GetKeyState(SDL_SCANCODE_DOWN) == Key_Down)
 	{
-		//预选位置向上移动
-		if (input->GetKeyState(SDL_SCANCODE_UP) == Key_Down)
+		yuxuan_point.y++;
+	}
+	//预选位置向左移动
+	else if (input->GetKeyState(SDL_SCANCODE_A) == Key_Down || 
+		input->GetKeyState(SDL_SCANCODE_LEFT) == Key_Down)
+	{
+		yuxuan_point.x--;
+	}
+	//预选位置向右移动
+	else if (input->GetKeyState(SDL_SCANCODE_D) == Key_Down || 
+		input->GetKeyState(SDL_SCANCODE_RIGHT) == Key_Down)
+	{
+		yuxuan_point.x++;
+	}
+	//选择
+	//选择结束时记得更新地图
+	else if (input->GetKeyState(SDL_SCANCODE_G) == Key_Down)
+	{
+		//判断是否是玩家的回合
+		if (map->Gethuihe() % 2 == flag)
 		{
-			yuxuan_point.y--;
-		}
-		//预选位置向下移动
-		else if (input->GetKeyState(SDL_SCANCODE_DOWN) == Key_Down)
-		{
-			yuxuan_point.y++;
-		}
-		//预选位置向左移动
-		else if (input->GetKeyState(SDL_SCANCODE_LEFT) == Key_Down)
-		{
-			yuxuan_point.x--;
-		}
-		//预选位置向右移动
-		else if (input->GetKeyState(SDL_SCANCODE_RIGHT) == Key_Down)
-		{
-			yuxuan_point.x++;
-		}
-		//选择
-		//选择结束时记得更新地图
-		else if (input->GetKeyState(SDL_SCANCODE_KP_1) == Key_Down)
-		{
-			//判断是否是玩家的回合
-			if (map->Gethuihe() % 2 == Blue_Player) 
+			//地图点位未被占用
+			if (map->GetMapState(&yuxuan_point).zhan_Point == NONE)
 			{
-				//地图点位未被占用
-				if (map->GetMapState(&yuxuan_point).zhan_Point == false)
-				{
-					//基地还没确定，优先确定基地
-					if (!jidi_point.x) {
-						map->Update(&yuxuan_point, Blue_Player);
-						jidi_point.x = yuxuan_point.x * 100 - 75;
-						jidi_point.y = yuxuan_point.y * 100 - 75;
-						before_point = yuxuan_point;
-						Point* point = new Point(jidi_point);
-						point->x += 25;
-						point->y += 25;
-						move_point.emplace_back(point);
-					}
-					else {
-						//判断玩家移动是否正确
-						switch (MoveIsRight()) {
-						case 1:
-							map->Update(&yuxuan_point,Blue_Player);
-							before_point = yuxuan_point;
-							move_point.emplace_back(ToMapPoint(&yuxuan_point));//将点位加入
-							break;
-						case TOO_LONG:
-							//提示玩家距离过长
-							if (flag_window == 0) {
-								flag_window++;
-								main_window->Player_Window(L"距离过长",
-									{ Getyuxuan_point().x,Getyuxuan_point().y + 20,100,30 });
-							}
-							break;
-						case THOUGH:
-							//提示玩家禁止穿线
-							if (flag_window == 0) {
-								flag_window++;
-								main_window->Player_Window(L"禁止穿线",
-									{ Getyuxuan_point().x,Getyuxuan_point().y + 20,100,30 });
-							}
-							break;
-						}
-					}
+				//基地还没确定，优先确定基地
+				if (!jidi_point.x) {
+					map->Update(&yuxuan_point, flag);
+					jidi_point.x = yuxuan_point.x * 100 - 75;
+					jidi_point.y = yuxuan_point.y * 100 - 75;
+					before_point = yuxuan_point;
+					Point* point = new Point(jidi_point);
+					point->x += 25;
+					point->y += 25;
+					move_point.emplace_back(point);//将点位加入
 				}
-				else
-				{
-					//点位被使用
-					if (flag_window == 0) {
-						flag_window++;
-						main_window->Player_Window(L"点位被使用",
-							{ Getyuxuan_point().x,Getyuxuan_point().y + 20,100,30 });
+				else {
+					switch (MoveIsRight()) {
+					case 1:
+						map->Update(&yuxuan_point, flag);
+						before_point = yuxuan_point;
+						move_point.emplace_back(ToMapPoint(&yuxuan_point));//将点位加入
+						break;
+					case TOO_LONG:
+						//提示玩家距离过长
+						if (flag_window == 0) {
+							flag_window++;
+							main_window->Player_Window(L"距离过长",
+								{ Getyuxuan_point().x,Getyuxuan_point().y + 20,100,30 });
+						}
+						break;
+					case THOUGH:
+						//提示玩家禁止穿线
+						if (flag_window == 0) {
+							flag_window++;
+							main_window->Player_Window(L"禁止穿线",
+								{ Getyuxuan_point().x,Getyuxuan_point().y + 20,100,30 });
+						}
+						break;
 					}
 				}
 			}
-			else {
+			else
+			{
+				//点位被占用
 				if (flag_window == 0) {
 					flag_window++;
-					//轮到敌人
-					main_window->Player_Window(L"轮到红方",
+					main_window->Player_Window(L"点位被使用",
 						{ Getyuxuan_point().x,Getyuxuan_point().y + 20,100,30 });
 				}
+			}
+		}
+		else {
+			if (flag_window == 0) {
+				flag_window++;
+				//轮到敌人
+				main_window->Player_Window(L"轮到敌人",
+					{ Getyuxuan_point().x,Getyuxuan_point().y + 20,100,30 });
 			}
 		}
 	}
